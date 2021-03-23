df7ecccfbd7fbfe91b5704642cd138c2544d7dc9
AlexandrDulovec <alexandr.dulovec@student.ossp.cz>
2021-03-23 11:39:26 +0100

    vytvoření souboru index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+
+// věk
+ $age = 14 ;
+
+
+// Větší nebo rovno
+if($age > 17) {
+ echo "Můžeš si udělat řidičský průkaz a vydají ti ho.";   
+}elseif($age  >16){
+ echo"Můžeš si udělat řidičský průkaz.";   
+} else 
+  echo " Nemůžeš si udělat řidičský průkaz."
+
+
+
+
+  ?>  
+</body>
+</html>
